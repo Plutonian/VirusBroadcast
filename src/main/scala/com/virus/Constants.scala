fe4c7f9afbd9fac573b86930318fa77afbea8492
benbear <benbear0519@gmail.com>
2020-02-08 21:45:08 +0900

    v2

--- src/main/scala/com/virus/Constants.scala
+++ src/main/scala/com/virus/Constants.scala
@@ -7,11 +7,11 @@ package com.virus
  * @date: 2020年02月02日 21:40
  */
 object Constants {
-  val PEOPLE = 1000
+  val PEOPLE = 5000
   val ORIGINAL_COUNT = 50 //初始感染数量
   val BROAD_RATE = 0.8f //传播率
   val SHADOW_TIME = 140 //潜伏时间
   val HOSPITAL_RECEIVE_TIME = 10 //医院收治响应时间
-  val BED_COUNT = 60 //医院床位
-  val u = -0.99f //流动意向平均值
+  val BED_COUNT = 1000 //医院床位
+  val u = 0.99f //流动意向平均值
 }
